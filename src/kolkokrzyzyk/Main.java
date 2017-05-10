7a4be4945d7f1da243c5bbdbe721b7b076771b35
Bartus <Bartus@mbp-sarata.home>
2017-05-10 16:27:02 +0200

    SnapshotTwo

--- src/kolkokrzyzyk/Main.java
+++ src/kolkokrzyzyk/Main.java
@@ -5,17 +5,53 @@
  */
 package kolkokrzyzyk;
 
+import java.awt.EventQueue;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 /**
  *
  * @author Bartus
  */
-public class Main {
+public class Main extends JFrame{
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    int counter =0;
+    
+    public Main(){
+       setSize(800,800);
+       setVisible(true);
+       setTitle("Kółko i krzyżyk");
+       setLayout(new GridLayout(3,3));
+       for(int i=1; i <=9; i++){
+            JButton button = new JButton("X");
+            add(button);
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    JButton button = (JButton)e.getSource();
+                    if(counter % 2 == 0){
+                        button.setText("X");
+                        System.out.println("X");
+                    }else{
+                        button.setText("O");
+                        System.out.println("O");
+                    }
+                    button.setEnabled(false);
+                    counter++;
+                }
+            });
+       }
     }
     
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable(){
+            @Override
+            public void run() {
+                new Main();
+            }
+        });    
+    } 
 }
